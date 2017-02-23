500969a601d0777f0b008574ffdf616d435f5ec1
Do Trong Thu <fewdspuckrito@gmail.com>
2017-02-23 17:47:09 +0700

    Add files via upload

--- src/vector.pas
+++ src/vector.pas
@@ -27,6 +27,9 @@ type
   end;
 
   Tvec3 = class
+    private
+      var hx,hy,hz: real;
+
     public
       constructor Create(tx: real = 0; ty: real = 0; tz: real = 0);
       constructor Create(t1: Tvec2; t2: real = 0);
@@ -34,7 +37,9 @@ type
       function Length: real;
       function Normalize: Tvec3;
 
-      var hx,hy,hz: real;
+      property X: real read hx write hx;
+      property Y: real read hy write hy;
+      property Z: real read hz write hz;
 
   end;
 
@@ -51,15 +56,15 @@ operator -(para1: Tvec2; para2: real) subf: Tvec2;
 operator *(para1: Tvec2; para2: real) mulf: Tvec2;
 operator /(para1: Tvec2; para2: real) dividef: Tvec2;
 
-operator +(para1, para2: Tvec2) add: Tvec2;
-operator -(para1, para2: Tvec2) sub: Tvec2;
-operator *(para1, para2: Tvec2) mul: Tvec2;
-operator /(para1, para2: Tvec2) divide: Tvec2;
+operator +(para1, para2: Tvec3) add: Tvec3;
+operator -(para1, para2: Tvec3) sub: Tvec3;
+operator *(para1, para2: Tvec3) mul: Tvec3;
+operator /(para1, para2: Tvec3) divide: Tvec3;
 
-operator +(para1: Tvec2; para2: real) addf: Tvec2;
-operator -(para1: Tvec2; para2: real) subf: Tvec2;
-operator *(para1: Tvec2; para2: real) mulf: Tvec2;
-operator /(para1: Tvec2; para2: real) dividef: Tvec2;
+operator +(para1: Tvec3; para2: real) addf: Tvec3;
+operator -(para1: Tvec3; para2: real) subf: Tvec3;
+operator *(para1: Tvec3; para2: real) mulf: Tvec3;
+operator /(para1: Tvec3; para2: real) dividef: Tvec3;
 
 function Dot(para1, para2: Tvec3): real;
 function Angle(para1, para2: Tvec3): real;
@@ -175,77 +180,85 @@ implementation
 
               operator + (para1, para2: Tvec3) add: Tvec3;
               begin
-                         add.hx := para1.hx + para2.hx;
-                         add.hy := para1.hy + para2.hy;
-                         add.hz := para1.hz + para2.hz;
+                         add := Tvec3.Create;
+                         add.X := para1.X + para2.X;
+                         add.Y := para1.Y + para2.Y;
+                         add.Z := para1.Z + para2.Z;
               end;
 
               operator - (para1, para2: Tvec3) sub: Tvec3;
               begin
-                         sub.hx := para1.hx - para2.hx;
-                         sub.hy := para1.hy - para2.hy;
-                         sub.hz := para1.hz - para2.hz;
+                         sub := Tvec3.Create;
+                         sub.X := para1.X - para2.X;
+                         sub.Y := para1.Y - para2.Y;
+                         sub.Z := para1.Z - para2.Z;
               end;
 
               operator * (para1, para2: Tvec3) mul: Tvec3;
               begin
-                         mul.hx := para1.hx * para2.hx;
-                         mul.hy := para1.hy * para2.hy;
-                         mul.hz := para1.hz * para2.hz;
+                         mul := Tvec3.Create;
+                         mul.X := para1.X * para2.X;
+                         mul.Y := para1.Y * para2.Y;
+                         mul.Z := para1.Z * para2.Z;
               end;
 
               operator / (para1, para2: Tvec3) divide: Tvec3;
               begin
-                         divide.hx := para1.hx / para2.hx;
-                         divide.hy := para1.hy / para2.hy;
-                         divide.hz := para1.hz / para2.hz;
+                         divide := Tvec3.Create;
+                         divide.X := para1.X / para2.X;
+                         divide.Y := para1.Y / para2.Y;
+                         divide.Z := para1.Z / para2.Z;
               end;
 
               operator + (para1:Tvec3 ; para2: real) addf: Tvec3;
               begin
-                         addf.hx := para1.hx + para2;
-                         addf.hy := para1.hy + para2;
-                         addf.hz := para1.hz + para2;
+                         addf := Tvec3.Create;
+                         addf.X := para1.X + para2;
+                         addf.Y := para1.Y + para2;
+                         addf.Z := para1.Z + para2;
               end;
 
               operator - (para1:Tvec3 ; para2: real) subf: Tvec3;
               begin
-                         subf.hx := para1.hx - para2;
-                         subf.hy := para1.hy - para2;
-                         subf.hz := para1.hz - para2;
+                         subf := Tvec3.Create;
+                         subf.X := para1.X - para2;
+                         subf.Y := para1.Y - para2;
+                         subf.Z := para1.Z - para2;
               end;
 
               operator * (para1:Tvec3 ; para2: real) mulf: Tvec3;
               begin
-                         mulf.hx := para1.hx * para2;
-                         mulf.hy := para1.hy * para2;
-                         mulf.hz := para1.hz * para2;
+                         mulf := Tvec3.Create;
+                         mulf.X := para1.X * para2;
+                         mulf.Y := para1.Y * para2;
+                         mulf.Z := para1.Z * para2;
               end;
 
               operator / (para1:Tvec3 ; para2: real) dividef: Tvec3;
               begin
-                         dividef.hx := para1.hx / para2;
-                         dividef.hy := para1.hy / para2;
-                         dividef.hz := para1.hz / para2;
+                         dividef := Tvec3.Create;
+                         dividef.X := para1.X / para2;
+                         dividef.Y := para1.Y / para2;
+                         dividef.Z := para1.Z / para2;
               end;
 
 
               function Tvec3.Length: real;
               begin
-                        Length:= Sqrt(Power(hx,2)+ Power(hy,2) + Power(hz,2));
+                        Length:= Sqrt(Power(X,2)+ Power(Y,2) + Power(Z,2));
               end;
 
               function Tvec3.Normalize: Tvec3;
               var tmp: real;
               begin
-                         tmp:= Sqrt(Power(hx,2)+ Power(hy,2) + Power(hz,2));
+                         tmp:= Sqrt(Power(X,2)+ Power(Y,2) + Power(Z,2));
                          Normalize:= self / tmp;
               end;
 
 
               function Dot(para1, para2: Tvec3): real;
               begin
-                            Dot := para1.hx * para2.hx + para1.hy + para2.hy + para1.hz * para2.hz;
+                            Dot := para1.X * para2.X + para1.Y + para2.Y + para1.Z * para2.Z;
               end;
 
               function Angle(para1, para2: Tvec3): real;
@@ -262,9 +275,9 @@ implementation
 
               function Cross(para1, para2: Tvec3): Tvec3;
               begin
-                            Cross.hx := para1.hy * para2.hz - para1.hz * para2.hy;
-                            Cross.hy := para1.hz* para2.hx - para1.hx * para2.hz;
-                            Cross.hz := para1.hx * para2.hy - para1.hy * para2.hx;
+                            Cross.X := para1.Y * para2.Z - para1.Z * para2.Y;
+                            Cross.Y := para1.Z* para2.X - para1.X * para2.Z;
+                            Cross.Z := para1.X * para2.Y - para1.Y * para2.X;
               end;
 
 end.
